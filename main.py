25cc11b6abfa103a049fe0afac79c2785513218c
Mohamed Fawzy <mohamedawzy400@gmail.com>
2026-07-27 10:42:24 +0300

    move application file into src directory

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 load_dotenv()
 
-from routes import base
+from src.routes import base
 
 app = FastAPI()
 app.include_router(base.base_router)
