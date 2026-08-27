b01571817f1e20d8207b781ba664ef7df8db987c
Mohamed Fawzy <mohamedawzy400@gmail.com>
2026-08-27 08:13:39 +0300

    fix: correct project chunks retrieval method

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -50,7 +50,7 @@ async def index_project(request: Request, project_id: int, push_request: PushReq
     
     while has_records:
         
-        page_chunks = await chunk_model.get_project_chunks(project_id=project.project_id,page_no=page_no)
+        page_chunks = await chunk_model.get_poject_chunks(project_id=project.project_id,page_no=page_no)
         
         if len(page_chunks):
             page_no += 1
