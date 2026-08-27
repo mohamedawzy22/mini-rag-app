fd7c52d3a3e9f63abb4a9be802bca43043c1980c
Mohamed Fawzy <mohamedawzy400@gmail.com>
2026-08-27 08:07:34 +0300

    refactor: migrate schemas from MongoDB to PostgreSQL

--- src/models/db_schema/minirag/schemes/asset.py
+++ src/models/db_schema/minirag/schemes/asset.py
@@ -15,15 +15,15 @@ class Asset(SQLAlchemyBase):
     asset_type = Column(String, nullable = False)
     asset_name = Column(String, nullable = False)
     asset_size = Column(Integer, nullable = False)
-    asset_config = Column(JSONB, nullable = False)
+    asset_config = Column(JSONB, nullable = True)
     
     asset_project_id = Column(Integer,ForeignKey("projects.project_id"), nullable = False)
     
     created_at = Column(DateTime(timezone=True),server_default = func.now(),nullable = False)
     updated_at = Column(DateTime(timezone=True),onupdate = func.now(),nullable = True)    
     
-    project = relationship("Project",back_populates="assets")
-    
+    projects = relationship("Project",back_populates="assets")
+    chunks = relationship("DataChunk",back_populates="assets")
     
     __table_args__ = (
         Index('ix_asset_project_id', asset_project_id),
